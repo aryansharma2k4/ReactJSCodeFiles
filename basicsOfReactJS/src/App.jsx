4590c5837d9865ac2262f9e32c699dc4be36df83
aryansharma2k4 <aryansharma2k4@gmail.com>
2024-03-04 22:10:55 +0530

    reactFlowAndStructure

---
+++ basicsOfReactJS/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Aryan from './aryan.jsx'
+
+function App() {
+  return(
+    <>
+    <Aryan/>
+    <p>This is a test para element</p>
+    </>
+  );
+}
+
+export default App
---
+++ basicsOfReactJS/src/aryan.jsx
@@ -0,0 +1,6 @@
+function Aryan(){
+      return (
+            <h1>Aryan Sharma</h1>
+      );
+}
+export default Aryan 
